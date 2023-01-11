ef70fff1718e882af2f132762ea019e7d7df15e8
Daniel <rdjmartinez95@gmail.com>
2023-01-11 09:55:41 -0300

    Theme button removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 import "./Header.css";
 const Header = () => {
   return (
     <span onClick={() => window.scrollTo(0, 0)} className="header">
       Movies and series
-      <Button onClick={() => handleTheme} className="theme">
+      {/* <Button onClick={() => handleTheme} className="theme">
         Dark
-      </Button>
+      </Button> */}
     </span>
   );
 };
